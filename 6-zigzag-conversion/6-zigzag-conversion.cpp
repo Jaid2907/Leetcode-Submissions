9b675c2ec8f2156c94b3eb1e7d5b931adaa248c1
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-02-09 15:26:02 +0530

    Time: 267 ms (5.00%), Space: 39.7 MB (10.94%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.cpp
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -36,7 +36,7 @@ public:
         for(int i=0; i<v.size(); i++)
         {
             for(int j=0; j<v[0].size(); j++)
-            {if(v[i][j] == '0')
+            {  if(v[i][j] == '0')
                     continue;
                     ans += v[i][j];
             }
