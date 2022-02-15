9330083660c3ce55a500b5e63478f9b6d2bfd825
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-02-15 09:48:25 +0530

    Time: 27 ms (53.51%), Space: 17 MB (50.83%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+     int xor_value = 0;
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            xor_value^=nums[i];
+        }
+        
+        return xor_value;
+    }
+};
