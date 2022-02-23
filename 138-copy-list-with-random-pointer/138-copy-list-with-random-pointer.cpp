9c6619f9eab5bd8d3f95e23a0af5c70410588f2a
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-02-23 13:02:35 +0530

    Time: 28 ms (6.00%), Space: 11.5 MB (16.56%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -34,17 +34,10 @@ public:
         
         vector<int> index(i);
         p = head;
-        Node* new_head = new Node(p->val);
-        Node* prev = new_head;
-        i = 0;
-        mp2[i] = new_head;
-        if(p->random == NULL)
-            index[i++] = -1;
-        else
-            index[i++] = mp[p->random];
-        
-        p=p->next;
-        
+        Node* prev = NULL, *new_head;
+        i = 0;    
+        int flag = 0;
+    
         while(p)
         {
             Node* t = new Node(p->val);
@@ -55,6 +48,14 @@ public:
                 
                 index[i] = mp[p->random];
             i++;
+            if(flag == 0)
+            {
+                new_head = t;
+                prev = t;
+                p = p->next;
+                flag = 1;
+                continue;
+            }
             prev->next = t;
             prev = t;
             p = p->next;
