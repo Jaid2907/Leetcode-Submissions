3982d5f72c4ccea7a71cc8a3a9fa580b35640df0
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-02-24 00:36:19 +0530

    Time: 479 ms (39.99%), Space: 155.3 MB (20.50%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -2,29 +2,41 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         
-    multiset<int> s;
-    
-    int n = nums.size();
-    vector<int> ans;
-    if(k>n)
-    {
-        ans.push_back(*max_element(nums.begin(),nums.end()));
+        vector<int> ans;
+        int n = nums.size();
+        deque <int> dq;
+        
+        for(int i=0;i<k; i++)
+        {
+            while(!dq.empty() && nums[i] > dq.back())
+                dq.pop_back();
+            
+            if(dq.empty() || nums[i]>dq.front())
+                dq.push_front(nums[i]);
+            else
+                dq.push_back(nums[i]);
+        }
+        
+        ans.push_back(dq.front());
+        
+        for(int i=k; i<n; i++)
+        {
+            if(!dq.empty() && dq.front() == nums[i-k])
+                dq.pop_front();
+            
+            while(!dq.empty() && nums[i] > dq.back())
+                dq.pop_back();
+            
+            if(dq.empty() || nums[i]>dq.front())
+                dq.push_front(nums[i]);
+            else
+                dq.push_back(nums[i]);
+            
+            ans.push_back(dq.front());
+            
+            
+        }
+        
         return ans;
-    }
-    for(int i=0; i<k; i++)
-    s.insert(nums[i]);
-
-    
-    ans.push_back(*s.rbegin());
-
-    for(int i=k; i<n; i++)
-    {
-        s.insert(nums[i]);
-        auto it = s.find(nums[i-k]);
-        s.erase(it);
-        ans.push_back(*s.rbegin());
-    }
-
-    return ans;
     }
 };
