345a4586b3e2174f8bfec09f83dea93ada9acb29
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-03-06 12:45:29 +0530

    Time: 80 ms (11.01%), Space: 19.6 MB (15.72%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
+    int mod = 1e9 +7;
+    long long int totalWays (int picked, int delivered, int n, vector<vector<int>>& dp)
+    {
+        if(picked == n && delivered == n)
+            return 1;
+        
+        if(picked > n || delivered > n || delivered > picked )
+            return 0;
+        
+        if(dp[picked][delivered] != -1)
+            return dp[picked][delivered];
+        
+        //Ways to pick
+        long long int waysToPick = (n-picked) * totalWays(picked+1, delivered, n, dp);
+        
+        long long int waysToDeliver = (picked-delivered) * totalWays(picked,delivered+1,n,dp);
+        
+        long long int ans = (waysToPick % mod + waysToDeliver % mod) % mod;
+        
+        return dp[picked][delivered] = ans;
+        
+    }
     int countOrders(int n) {
         
-        long long int ans = 1;
-        int mod = 1e9+7;
         
-        for(int i=1; i<=n; i++)
-        {
-            ans = ( (ans%mod) * (i%mod) )%mod;
-        }
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
         
-        for(int i=1; i<=2*n-1; i+=2)
-        {
-            ans = ( (ans%mod) * (i%mod) ) %mod;
-        }
-        
-        return ans;
+        return totalWays(0,0,n,dp);
     }
 };
