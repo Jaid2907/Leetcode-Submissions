ed18b9a1382157b45f311588fbbfefc941ead266
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-06-16 11:58:43 +0530

    Time: 152 ms (36.57%), Space: 14.5 MB (37.02%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
-    int helper (int target, vector<int>& nums, vector<int>& dp){
 
-    if(target == 0){
-        return 0;
-    }
-    if(dp[target] != -1)
-        return dp[target];
-    
-    int ans = INT_MAX;
-    for(int i=0; i<nums.size(); i++){
-        if(target-nums[i]>=0)
-        ans =  min(ans,helper(target-nums[i],nums,dp));
-    }
-    
-    if(ans == INT_MAX)
-        return dp[target] = INT_MAX;
-    return dp[target] = ans+1;
-}
     int coinChange(vector<int>& num, int x) {
-    vector<int> dp(x+1,-1);
-    int ans = helper(x,num,dp);
-    if(ans == INT_MAX)
-        return -1;
-    return ans;
+    vector<int> dp(x+1,0);
+    unordered_map<int,int> mp;
+        
+    int n = num.size();
+        
+    for(auto it: num){
+        mp[it]++;
+    }
+        
+    for(int i=0; i<=x; i++){
+        if(mp.find(i) != mp.end())
+            dp[i] = 1;
+    }
+        
+    int ans = INT_MAX;
+        
+    for(int i=1; i<=x; i++){
+        ans = INT_MAX;
+        
+        for(int j=0; j<n; j++){
+            if(i-num[j]>=0)
+            ans = min(ans, dp[i-num[j]]);
+        }
+        
+        if(ans == INT_MAX)
+            dp[i] = INT_MAX;
+        else
+            dp[i] = 1+ans;
+        
+    }
+        
+    return dp[x] == INT_MAX?-1:dp[x];
     }
 };
