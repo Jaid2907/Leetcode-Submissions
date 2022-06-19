3633cb917109c75f462793310171fa3307f2257e
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-06-19 18:43:56 +0530

    Time: 521 ms (29.82%), Space: 106.6 MB (41.90%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,32 +1,8 @@
-class Graph{
-    public:
-    int V;
-    list<int>* l;
-    
-    Graph(int V){
-        this->V = V;
-        l = new list<int>[V];
-    }
-    
-    void addEdge(int u, int v){
-        l[u].push_back(v);
-    }
-    
-    void dfs(int i, vector<int>& visited){
-        
-        visited[i] = 1;
-        
-        for(auto it: l[i]){
-            if(!visited[it]){
-                dfs(it,visited);
-            }
-        }
-    }
-};
+
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        Graph g(n);
+       
         vector<int> temp(n,0);
         
         for(auto it: edges){
