b006197b397ae98cf0d04d3101182c1a8a97b271
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-06-19 17:41:40 +0530

    Time: 55 ms (76.10%), Space: 32.4 MB (61.11%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -7,24 +7,27 @@ public:
         q.push(start);
         visited[start] = 1;
         
+        
         while(!q.empty()){
+            
             auto node = q.front();
             q.pop();
             
             if(arr[node] == 0)
                 return true;
             
-            if(node + arr[node]>=0 && node + arr[node] < n && !visited[node + arr[node]]){
-                visited[node+arr[node]] = 1;
-                q.push(node+arr[node]);
+            if(node + arr[node] <n && !visited[node + arr[node]]){
+                q.push(node + arr[node]);
+                visited[node + arr[node]] = 1;
             }
             
-            if(node-arr[node]>=0 && node-arr[node]<n && !visited[node-arr[node]]){
-                visited[node-arr[node]] = 1;
-                q.push(node-arr[node]);
+            if(node - arr[node] >=0 && !visited[node - arr[node]]){
+                q.push(node - arr[node]);
+                visited[node - arr[node]] = 1;
             }
         }
         
         return false;
+        
     }
 };
