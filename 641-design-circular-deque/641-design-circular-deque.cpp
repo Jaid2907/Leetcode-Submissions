d43fef51b39965ce8baf983b0168c5ad8f8aec0a
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-07-01 16:19:32 +0530

    Time: 64 ms (8.78%), Space: 17.1 MB (8.92%) - LeetHub

---
+++ 641-design-circular-deque/641-design-circular-deque.cpp
@@ -0,0 +1,95 @@
+class MyCircularDeque {
+public:
+    list<int> queue;
+    int  k;
+    
+    
+    
+    MyCircularDeque(int k) {
+        
+        this->k = k;
+        
+    }
+    
+    bool insertFront(int value) {
+        
+        if(!isFull()){
+            queue.push_front(value);
+            return true;
+        }
+        return false;
+        
+    }
+    
+    bool insertLast(int value) {
+        
+        if(!isFull()){
+            queue.push_back(value);
+            
+            return true;
+        }
+        
+        return false;
+        
+    }
+    
+    bool deleteFront() {
+        
+        if(!isEmpty()){
+            queue.pop_front();
+        
+            return true;
+        }
+        
+        return false;
+    }
+    
+    bool deleteLast() {
+        if(!isEmpty()){
+            queue.pop_back();
+            
+            return true;
+        }
+        
+        return false;
+        
+    }
+    
+    int getFront() {
+        
+        if(!isEmpty()){
+            return queue.front();
+        }
+        return -1;
+    }
+    
+    int getRear() {
+        
+        if(!isEmpty()){
+            return queue.back();
+        }
+        return -1;
+        
+    }
+    
+    bool isEmpty() {
+     return queue.size() == 0;   
+    }
+    
+    bool isFull() {
+        return queue.size() == k;
+    }
+};
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque* obj = new MyCircularDeque(k);
+ * bool param_1 = obj->insertFront(value);
+ * bool param_2 = obj->insertLast(value);
+ * bool param_3 = obj->deleteFront();
+ * bool param_4 = obj->deleteLast();
+ * int param_5 = obj->getFront();
+ * int param_6 = obj->getRear();
+ * bool param_7 = obj->isEmpty();
+ * bool param_8 = obj->isFull();
+ */
