08b64eebc4a74f5687ef94b9443318b40aaf4b6d
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-07-01 10:44:36 +0530

    Time: 15 ms (29.65%), Space: 17.3 MB (6.74%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
+    int helper (int i, vector<int>& prices, bool bought,vector<vector<int>>& dp){
+        
+        if(i == prices.size())
+            return 0;
+        
+        if(dp[i][bought] != -1)
+            return dp[i][bought];
+        
+        if(bought){
+            int a = INT_MIN, b = INT_MIN;
+            
+            a = helper (i+1,prices,bought,dp);
+            
+            bought = false;
+            b = prices[i] + helper(i+1,prices,bought,dp);
+            
+            return dp[i][true] =  max(a,b);
+        }
+        else{
+            int a = INT_MIN, b = INT_MIN;
+            
+            a = helper(i+1,prices,bought,dp);
+            
+            bought = true;
+            b = -prices[i] + helper(i+1,prices,bought,dp);
+            
+            return dp[i][false] = max(a,b);
+
+        }
+    }
     int maxProfit(vector<int>& prices) {
         
         int n = prices.size();
-        int mini = prices[0];
-        int maxi = INT_MIN;
-        int profit = 0;
+        vector<vector<int>> dp(n, vector<int> (2,-1));
         
-        
-        for(int i=0; i<n; i++){
-            
-            if(prices[i] - mini > 0){
-                profit += prices[i]-mini;
-                mini = prices[i];
-                continue;
-            }
-            
-            mini = min (mini, prices[i]);
-            
-            
-        }
-        
-        // if(prices[n-1]-mini>0)
-        //     profit += prices[n-1]-mini;
-        return profit;
+        return helper(0,prices,false,dp);
     
     }
     
