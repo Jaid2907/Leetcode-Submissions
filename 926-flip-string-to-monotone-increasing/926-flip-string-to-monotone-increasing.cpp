394c69aa5ac82fb7ec00945e36b354e730ea103c
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-07-02 18:46:58 +0530

    Time: 35 ms (65.93%), Space: 11 MB (78.13%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
@@ -5,31 +5,25 @@ public:
         int n = s.size();
         if(n == 1)
             return 0;
-        vector<int> prefix_1(n+1,0);
-        vector<int> prefix_0(n+1,0);
+        int cnt_1 = 0, cnt_0  = 0;
         
-        int cnt_0 = 0, cnt_1 = 0;
         
         for(int i=0; i<n; i++){
-            if(s[i] == '1'){
-                cnt_1++;
-            }
-            prefix_1[i] = cnt_1;
-        }
-        
-        for(int i=n-1; i>=0; i--){
-            if(s[i] == '0'){
+            if(s[i] == '0')
                 cnt_0++;
-            }
-            
-            prefix_0[i] = cnt_0;
         }
         
-        int ans = prefix_0[1];
-        
-        for(int i=1; i<n; i++){
+        int ans = cnt_0;
+        for(int i=0; i<n; i++){
             
-            ans = min (ans, prefix_0[i+1] + prefix_1[i-1]);
+            if(s[i] == '0')
+                cnt_0--;
+             
+            
+            ans = min (ans,cnt_1 + cnt_0);
+            if(s[i] == '1')
+                cnt_1++;
+           
            
         }
         
