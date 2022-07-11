815bf6b251d013df01791ac327878ebde6313244
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-07-11 20:59:41 +0530

    Time: 3 ms (82.53%), Space: 7 MB (50.65%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -33,9 +33,29 @@ public:
         
         if(s3.size() != (s1.size() + s2.size()))
             return false;
-        vector<vector<int>> dp (s1.size(), vector<int>(s2.size(),-1));
+        vector<vector<int>> dp (s1.size()+1, vector<int>(s2.size()+1,-1));
         
-        return helper(0,0,0,s1,s2,s3,dp);
+        for(int i=s1.size(); i>=0; i--){
+            for(int j=s2.size(); j>=0; j--){
+                int k = i+j;
+                
+                if(i == s1.size() && j == s2.size())
+                    dp[i][j] = 1;
+                else if(s1[i] == s3[k] && s2[j] == s3[k])
+                    dp[i][j] = dp[i+1][j] || dp[i][j+1];
+                else if(s1[i] == s3[k])
+                    dp[i][j] = dp[i+1][j];
+                else if(s2[j] == s3[k])
+                    dp[i][j] = dp[i][j+1];
+                else 
+                    dp[i][j] = false;
+                
+            }
+        }
+        
+        return dp[0][0];
+        
+        
         
     }
 };
