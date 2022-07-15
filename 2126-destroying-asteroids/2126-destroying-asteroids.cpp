552712085e903c4d4391ea8bc8fa771849fd4f21
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-07-15 10:09:28 +0530

    Time: 283 ms (68.59%), Space: 102.9 MB (42.94%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort (asteroids.begin(), asteroids.end());
+        int n = asteroids.size();
+        long long int m = mass;
+        
+        for(int i=0; i<n; i++){
+            
+            if(asteroids[i] <= m){
+                m += asteroids[i];
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+};
