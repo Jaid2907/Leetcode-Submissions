a8e0fa5d6820540a2b3349337eaafa2c072f6256
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-07-15 10:01:01 +0530

    Time: 71 ms (55.99%), Space: 14.6 MB (52.41%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.cpp
+++ 85-maximal-rectangle/85-maximal-rectangle.cpp
@@ -57,102 +57,20 @@ public:
         int n = matrix[0].size();
         if(m == 1 && n == 1)
             return matrix[0][0] == '1';
-        vector<vector<pair<int,int>>> dup(m,vector<pair<int,int>>(n));
-        
-       for(int i = 0; i<m; i++){
-           stack<pair<char,int>> st;
-           
-           for(int j=n-1; j>=0; j--){
-               
-               while(!st.empty() && st.top().first == '1')
-                   st.pop();
-               
-               if(!st.empty() && st.top().first == '0'){
-                   dup[i][j].second = st.top().second;
-               }
-               else{
-                   dup[i][j].second = n;
-               }
-               
-               st.push({matrix[i][j],j});
-           }
-       }
-        
-        
-        for(int j = 0; j<n; j++){
-            
-            stack<vector<int>> st;
-            int a,b;
-            vector<int> down(m), up(m);
-                
-            for(int i = m-1; i>=0; i--){
-                
-                
-                    while(!st.empty() && st.top()[0] == 1 && st.top()[1] >= dup[i][j].second)
-                    st.pop();
-                
-                    if(!st.empty()){
-                        down[i] = st.top()[2];
-                    }
-                    else{
-                        down[i] = m;
-                    }   
-                st.push({matrix[i][j] == '0'? 0:1 ,dup[i][j].second , i});
-                   
-            }
-            
-            while(!st.empty())
-                st.pop();
-            
-            for(int i = 0; i<m; i++){
-                
-                while(!st.empty() && st.top()[0] == 1 && st.top()[1] >= dup[i][j].second)
-                    st.pop();
-                
-                if(!st.empty() ){
-                    up[i] = st.top()[2];
-                }
-                else{
-                    up[i] = -1;
-                }
-                
-                st.push({matrix[i][j] == '0'? 0:1 ,dup[i][j].second , i});
-                
-                
-            }
-            
-            for(int i=0; i<m; i++){
-                
-                dup[i][j].first = down[i]-up[i]-1;
-            }
-            
-        }
-            
-        
-    
-        
-
+        vector<int> heights(n,0);
         int ans = 0;
         
         for(int i=0; i<m; i++){
+            
             for(int j=0; j<n; j++){
-                if(matrix[i][j] == '1')
-                {
-                     ans = max(ans, (dup[i][j].first * (dup[i][j].second-j)));
-                }
-               
+                if(matrix[i][j] == '0')
+                    heights[j] = 0;
+                else
+                    heights[j]++;
             }
+            
+            ans = max(ans,largestRectangleArea(heights));
         }
-        
-//         for(int i=0; i<m; i++){
-//             for(int j=0; j<n; j++){
-//                     cout<<"("<<dup[i][j].first<<","<<dup[i][j].second<<")"<<" ";
-                
-//             }
-//             cout<<endl;
-//         }
-        
-      
         
         return ans;
     }
