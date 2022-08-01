59f6b6015371905178348d9b81208db3872b9b2e
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-08-01 21:52:59 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (52.83%) - LeetHub

--- 765-couples-holding-hands/765-couples-holding-hands.cpp
+++ 765-couples-holding-hands/765-couples-holding-hands.cpp
@@ -1,34 +1,83 @@
+class DSU {
+    public:
+    vector<int> rank;
+    vector<int> parent;
+    
+    DSU(int n){
+        
+        rank.resize(n,0);
+        parent.resize(n);
+        
+        for(int i = 0; i<n; i++){
+            parent[i] = i;
+        }
+        
+    }
+    void set_parent(int child, int par){
+        
+        parent[child] = par;
+    }
+    
+    int find(int i){
+        
+        if(parent[i] == i)
+            return i;
+        
+        return find(parent[i]);
+    }
+    
+    void union_set(int x, int y){
+        
+        int s1 = find(x);
+        int s2 = find(y);
+        
+        if(s1 != s2){
+            if(rank[s1]<rank[s2]){
+                parent[s1] = s2;
+                rank[s2] += rank[s1];
+            }
+            else{
+                parent[s2] = s1;
+                rank[s1] += rank[s2];
+            }
+        }
+    }
+    
+    bool diff_set(int x, int y){
+        
+        int s1 = find(x);
+        int s2 = find(y);
+        
+        if(s1 == s2)
+            return false;
+        parent[s1] = s2;
+        
+        return true;
+    }
+    
+};
 class Solution {
 public:
     int minSwapsCouples(vector<int>& row) {
-        unordered_map<int,int> mp;
+        int n = row.size();
+        DSU ds(n);
+        int cnt = 0;
         
-        for(int i = 0; i<row.size(); i++){
-            mp[row[i]] = i;
+        
+        for(int i = 0; i<n; i+=2){
+            
+            ds.set_parent(row[i], row[i]);
+            ds.set_parent(row[i+1], row[i]);
         }
         
-        int cnt =  0;
-        int s = row.size();
-        
-        for(int i = 0; i<s; i+=2){
+        for(int i = 0; i<n; i+=2){
             
-            int first = row[i];
-            int second = first^1;
-            
-            if(row[i+1] != second){
-                
-                int j = mp[second];
-                int prev = row[i+1];
-                swap(row[i+1], row[mp[second]]);
-                mp[row[i+1]] = i+1;
-                mp[prev] = j;
+            if(ds.diff_set(i,i+1)){
                 cnt++;
             }
-            
         }
         
         return cnt;
-        
         
     }
 };
