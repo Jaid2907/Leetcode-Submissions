8287d1834361235a63aa502cb7bb0d645af54c01
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-08-14 18:05:24 +0530

    Time: 33 ms (49.06%), Space: 19.5 MB (24.87%) - LeetHub

--- 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
@@ -4,28 +4,30 @@ public:
         int cnt = 0;
         int ans = 0;
         int n = arr.size();
+        vector<int> forward(n,1), backward(n,1);
         
-        for(int i = 0; i<n; i++){
-            
-            if(i>0 && i+1<n && arr[i]>arr[i-1] && arr[i]>arr[i+1]){
-                cnt = 0;
-                int j = i;
-                while(j>0 && arr[j]>arr[j-1]){
-                    cnt++;
-                    j--;
-                }
-                j = i;
-                
-                while(j<n-1 && arr[j]>arr[j+1]){
-                    cnt++;
-                    j++;
-                }
-                
-                ans = max(ans, cnt+1);
-                    
+        for(int i = 1; i<n; i++){
+            if(arr[i] > arr[i-1]){
+                forward[i] = forward[i-1]+1;
+            }
+        }
+        for(int j = n-2; j>=0; j--){
+            if(arr[j] > arr[j+1]){
+                backward[j] = backward[j+1] + 1;
             }
         }
         
+        for(int i = 0; i<n; i++){
+            if(forward[i] != 1 && backward[i] != 1)
+            ans = max(ans, forward[i] + backward[i] - 1);
+        }
+        
+        // for(int i = 0; i<n; i++)
+        //     cout<<forward[i]<<" ";
+        // cout<<endl;
+        // for(int j = 0; j<n; j++)
+        //     cout<<backward[j]<<" ";
+        // cout<<endl;
         return ans;
     }
 };
