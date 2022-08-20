fcfeb1a90689d36b86dd9af89ee6d7dcd3c61313
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-08-20 12:55:15 +0530

    Time: 498 ms (48.58%), Space: 97.1 MB (72.44%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -1,45 +1,20 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
-        int max_col = 0;
-        int m = nums.size();
+        int r = nums.size();
+        unordered_map<int,vector<int>> mp;
+        vector<int> ans;
         
-        for(int i = 0; i<m; i++){
-            if(max_col < nums[i].size()){
-                max_col = nums[i].size();
+        for(int i = r-1; i>=0; i--){
+            
+            for(int j = 0; j<nums[i].size(); j++){
+                mp[i + j].push_back(nums[i][j]);
             }
-           
         }
         
-        map <string, int> mp;
-        queue<pair<int,int>> q;
-        vector<int> ans;
-        q.push({0,0});
-        string key = to_string(0) + "*" + to_string(0);
-        mp[key] = 1;
-        
-        while(!q.empty()){
-            
-            int size = q.size();
-            
-            while(size--){
-                auto node = q.front();
-                q.pop();
-                
-                
-                
-                ans.push_back(nums[node.first][node.second]);
-                key = to_string(node.first + 1 ) + "*" + to_string(node.second);
-                if(node.first + 1 < m && node.second < nums[node.first + 1].size() && !mp[key]){
-                    q.push({node.first + 1, node.second});
-                    mp[key] = 1;
-                }
-                
-                key = to_string(node.first) + "*" + to_string(node.second + 1);
-                if(node.second + 1 <nums[node.first].size() && !mp[key]){
-                    q.push({node.first, node.second + 1});
-                    mp[key] = 1;
-                }
+        for(int i = 0; i<mp.size(); i++){
+            for(auto &it: mp[i]){
+                ans.push_back(it);
             }
         }
         
