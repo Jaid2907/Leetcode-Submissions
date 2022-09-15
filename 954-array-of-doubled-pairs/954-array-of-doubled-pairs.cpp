a5273b8028791a155d2c573a1076b450b8a15254
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-09-15 14:16:56 +0530

    Time: 1042 ms (5.00%), Space: 58 MB (80.94%) - LeetHub

---
+++ 954-array-of-doubled-pairs/954-array-of-doubled-pairs.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    static bool compare (int a, int b){
+        if(a<0 && b<0)
+            return a>b;
+        return a<b;
+    }
+    bool canReorderDoubled(vector<int>& changed) {
+        
+        
+        
+        unordered_map<int,int> mp;
+        
+        int n = changed.size();
+        int f = 0;
+       
+        for(int i = 0; i<n; i++){
+            mp[changed[i]]++;
+        }
+        sort(changed.begin(), changed.end(), compare);
+        
+        for(auto it: changed){
+            cout<<it<<" ";
+        }
+        for(int i = 0; i<n; i++){
+            if(mp.find(changed[i]) != mp.end()){
+                if(mp.find(2*changed[i]) == mp.end()){
+                    f = 1;
+                    break;
+                }
+                
+               
+                mp[changed[i]]--;
+                if(mp[changed[i]] == 0)
+                    mp.erase(changed[i]);
+                
+                mp[2*changed[i]]--;
+                if(mp[2*changed[i]] == 0)
+                    mp.erase(2*changed[i]);
+            }
+        }
+        
+        if(mp.empty() && f == 0)
+            return true;
+        return false;
+    
+    }
+};
