29d636409074224c6abcd8afd5e800140eb05d45
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-09-24 09:38:34 +0530

    Time: 30 ms (95.72%), Space: 5.9 MB (47.06%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -14,14 +14,15 @@ public:
     int concatenatedBinary(int n) {
         
         long long int ans = 0;
+        int l = 0;
         int mod = 1e9+7;
+        
         for(int i = 1; i<=n; i++){
             
-            int j = decimalToBinary(i);
+            if((i & (i-1)) == 0) l++;
             
-            ans = ans<<j;
-            ans %= mod;
-            ans += i;
+            ans = ans<<l;
+            ans |= i;
             ans %= mod;
             
         }
