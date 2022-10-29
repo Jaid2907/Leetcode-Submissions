8fdfb2d70897c8de9a4f093a9984a66d8bcfb109
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-10-29 17:24:56 +0530

    Added solution - LeetHub

---
+++ Phone directory - GFG/phone-directory.cpp
@@ -0,0 +1,121 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Node{
+public:
+  set<string> v;
+  vector<Node*> l;
+  bool flag;
+  
+  
+  Node(){
+      l.resize(26,NULL);
+      v.clear();
+      flag = false;
+  }
+};
+class Trie{
+    public:
+  Node* root;
+  Trie(){
+      root = new Node();
+  }
+  
+  void insert(string& s){
+      int n = s.size();
+      Node* ptr = root;
+      
+      
+      for(int i = 0; i<n; i++){
+          if(ptr->l[s[i]-97] == NULL){
+              ptr->l[s[i]-97] = new Node();
+              ptr = ptr->l[s[i]-97];
+              ptr->v.insert(s);
+          }
+          else{
+              ptr = ptr->l[s[i]-97];
+              ptr->v.insert(s);
+              
+          }
+          
+      }
+      
+      ptr->v.insert(s);
+      ptr->flag = true;
+  }
+  
+  vector<string> search(string& word){
+      
+      int n = word.size();
+      Node* ptr = root;
+      
+      for(int i = 0; i<n; i++){
+          if(ptr->l[word[i]-97] == NULL)
+            return {"0"};
+        ptr = ptr->l[word[i]-97];
+      }
+      
+      vector<string> ans;
+      for(auto &it: ptr->v){
+          ans.push_back(it);
+      }
+      
+      return ans;
+      
+  }
+    
+};
+class Solution{
+public:
+    vector<vector<string>> displayContacts(int n, string contact[], string s)
+    {
+        vector<vector<string>> ans;
+        Trie* t = new Trie();
+        
+        for(int i = 0; i<n; i++){
+            t->insert(contact[i]);
+        }
+        
+        string str = "";
+        for(int i = 0; i<s.size(); i++){
+            
+            str += s[i];
+            
+            ans.push_back(t->search(str));
+            
+        }
+        
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string contact[n], s;
+        for(int i = 0;i < n;i++)
+            cin>>contact[i];
+        cin>>s;
+        
+        Solution ob;
+        vector<vector<string>> ans = ob.displayContacts(n, contact, s);
+        for(int i = 0;i < s.size();i++){
+            for(auto u: ans[i])
+                cout<<u<<" ";
+            cout<<"\n";
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
