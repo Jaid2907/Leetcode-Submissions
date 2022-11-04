a9a8bdba21e9dcb2335df07b87651cf4be6e39c8
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-11-04 15:39:53 +0530

    Added solution - LeetHub

--- Base Equivalence - GFG/base-equivalence.cpp
+++ Base Equivalence - GFG/base-equivalence.cpp
@@ -7,21 +7,36 @@ using namespace std;
 // User function Template for C++
 class Solution {
   public:
-    string baseEquiv(int n, int m){
-        // code here
-        
-        for(int base = 2; base<=32; base++){
-            int num = n;
-            int cnt = 0;
-            while(num){
-                num /= base;
-                cnt++;
-            }
-            
-            if(cnt == m)
-                return "Yes";
+    int compare (int num, int mid){
+        int cnt = 0;
+        while(num){
+            num /= mid;
+            cnt++;
         }
         
+        return cnt;
+            
+    }
+    string baseEquiv(int n, int m){
+        // code here
+        int low = 2, high = 32;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            
+            int res = compare(n,mid);
+            if(res == m)
+                return "Yes";
+            if(res > m){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+            
+        }
+        
+       
         return "No";
     }
 };
