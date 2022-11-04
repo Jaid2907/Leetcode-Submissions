b6301f2a5e0b6d67fa9a903ec4fec6af3febf9cb
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2022-11-04 15:34:50 +0530

    Added solution - LeetHub

---
+++ Base Equivalence - GFG/base-equivalence.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string baseEquiv(int n, int m){
+        // code here
+        
+        for(int base = 2; base<=32; base++){
+            int num = n;
+            int cnt = 0;
+            while(num){
+                num /= base;
+                cnt++;
+            }
+            
+            if(cnt == m)
+                return "Yes";
+        }
+        
+        return "No";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        Solution ob;
+        cout<<ob.baseEquiv(n,m)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
