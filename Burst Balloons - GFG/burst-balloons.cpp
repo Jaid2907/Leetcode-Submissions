60ec4b0497ff90d899aeaa5d6aa9cd9c3f0813f0
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2023-05-31 13:09:26 +0530

    Added solution - LeetHub

--- Burst Balloons - GFG/burst-balloons.cpp
+++ Burst Balloons - GFG/burst-balloons.cpp
@@ -10,14 +10,10 @@ public:
         // code here
         vector<vector<int>> dp(n,vector<int>(n,0));
         
-        for(int i = 0; i<n; i++){
-            int lvalue = i-1 >=0 ? arr[i-1] : 1;
-            int rvalue = i+1<n ? arr[i+1] : 1;
-            dp[i][i] = arr[i] * lvalue* rvalue;
-        }
+       
         
         for(int i = n-1; i>=0; i--){
-            for(int j = i+1; j<n; j++){
+            for(int j = i; j<n; j++){
                 
                for(int k = i; k<=j; k++){
                 
@@ -34,11 +30,6 @@ public:
             }
         }
         
-        // for(int i = 0; i<n; i++){
-        //     for(int j = 0; j<n; j++)
-        //         cout<<dp[i][j]<<" ";
-        //     cout<<endl;
-        // }
        
         return dp[0][n-1];
     }
