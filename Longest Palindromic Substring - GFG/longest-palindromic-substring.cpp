b35c4cdf6ff8d135eafca3fa3fb35d2e3cb39e8d
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2023-06-01 16:52:59 +0530

    Added solution - LeetHub

---
+++ Longest Palindromic Substring - GFG/longest-palindromic-substring.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    string longestPalindrome(string s){
+        // code here 
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n,0));
+        int start = -1, end = -1, maxi = INT_MIN;
+        
+        for(int i = n-1; i>=0; i--){
+            for(int j = i; j<n; j++){
+                
+                if(j-i == 0){
+                    dp[i][j] = 1;
+                }
+                else if(j-i == 1){
+                    dp[i][j] = s[i]==s[j] ? 2 : 0;
+                }
+                else{
+                    if(s[i] == s[j] && dp[i+1][j-1] > 0)
+                        dp[i][j] = 2 + dp[i+1][j-1];
+                }
+                
+                if(dp[i][j] >= maxi){
+                    maxi = dp[i][j];
+                    start = i;
+                    end = j;
+                }
+            }
+        }
+        
+        
+        return s.substr(start,end-start+1);
+    }
+};
+
+//{ Driver Code Starts.
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string S;
+        cin>>S;
+        Solution ob;
+        cout<<ob.longestPalindrome(S)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
