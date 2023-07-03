51ae4b7abd70476bdaf5178d7130f3202ee67def
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2023-07-03 11:00:23 +0530

    Added solution - LeetHub

--- Maximum Index - GFG/maximum-index.cpp
+++ Maximum Index - GFG/maximum-index.cpp
@@ -8,35 +8,26 @@ class Solution{
 public:
     int maxIndexDiff(int arr[], int n) {
         // code here
-        vector<int> prefix;
-        int mini = arr[0];
-        prefix.push_back(arr[0]);
-        int maxi = 0;
-        
-        for(int i = 1; i<n; i++){
-            
-            int low = 0, high = prefix.size()-1;
-            int ans = -1;
-            
-            while(low <= high){
-                int mid = low + (high-low)/2;
-                
-                if(prefix[mid] <= arr[i]){
-                    ans = mid;
-                    high = mid - 1;
-                }
-                else{
-                    low = mid+1;
-                }
+        stack<int> st;
+        int ans = 0;
+        for(int i = 0; i<n; i++){
+            if(st.empty() || arr[st.top()] > arr[i]){
+                st.push(i);
             }
-            
-            if(ans != -1)
-                maxi = max(maxi,i-ans);
-            mini = min(mini,arr[i]);
-            prefix.push_back(mini);
         }
         
-        return maxi;
+        for(int i = n-1; i>=0; i--){
+            
+            while(!st.empty() && st.top() > i)
+                st.pop();
+                
+            while(!st.empty() && arr[i] >= arr[st.top()]){
+                ans = max(ans, i-st.top());
+                st.pop();
+            }
+        }
+        
+        return ans;
         
         
     }
