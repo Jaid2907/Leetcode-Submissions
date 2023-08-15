2d4d74f6419d2bbaef2bd1fde7f166f11c4dd34f
Jaid2907 <75538081+Jaid2907@users.noreply.github.com>
2023-08-15 16:28:05 +0530

    Added solution - LeetHub

---
+++ Largest Number formed from an Array - GFG/largest-number-formed-from-an-array.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// The main function that returns the arrangement with the largest value as
+	// string.
+	// The function accepts a vector of strings
+	static bool compare(string& a, string& b){
+	    string first = a+b;
+	    string second = b+a;
+	    return first > second;
+	}
+	string printLargest(vector<string> &arr) {
+	    // code here
+	    int n = arr.size();
+	    string ans = "";
+	    sort(arr.begin(), arr.end(),compare);
+	    
+	    for(int i = 0; i<n; i++){
+	        ans += arr[i];
+	    }
+	    
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        vector<string> arr(n);
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.printLargest(arr);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
